60c1073d1f9fff1b3d322e985e0cc22c4570fe0b
U-UPFUL\alex <sashaanderson@users.noreply.github.com>
2021-06-18 08:40:31 -0400

    feat: qrcat: Transfer data using QR codes.

--- qrcat/qrcat.groovy
+++ qrcat/qrcat.groovy
@@ -45,7 +45,7 @@ class SendChunker {
         if (bytes) {
             contents = bytes.encodeBase64().toString()
         } else {
-            contents = "\004";
+            contents = "\004"
             eof = true
         }
 
@@ -59,28 +59,45 @@ def send() {
     def sendChunker = new SendChunker()
     sendChunker.readChunk()
 
-    new SwingBuilder().edt {
-        frame(title: "qrcat", defaultCloseOperation: JFrame.EXIT_ON_CLOSE, pack: true, show: true) {
+    def t = 0L
+
+    def swing = new SwingBuilder()
+    swing.edt {
+        frame(title: "qrcat", defaultCloseOperation: JFrame.EXIT_ON_CLOSE, pack: true, show: true, id: "frame") {
             borderLayout()
             label(
                 icon: bind(source: sendChunker, sourceProperty: 'imageIcon'),
                 mouseClicked: { e ->
-                    if (e.getClickCount() == 2) {
+                    if (e.getClickCount() == 1) {
                         if (sendChunker.eof) {
                             System.err.println()
                             System.exit(0)
                         }
                         System.err.print('.')
                         sendChunker.readChunk()
+                        t = System.currentTimeMillis()
                     }
                 }
             )
         }
     }
+
+    def r = new Random()
+    new Timer(3000, {
+        if (t > 0 && System.currentTimeMillis() - t > 3000) {
+            def p = swing.frame.getLocationOnScreen()
+            p.x += (p.x < 20 ? 1 : p.x > 100 ? -1 : r.nextBoolean() ? 1 : -1) * r.nextInt(10)
+            p.y += (p.y < 20 ? 1 : p.y > 100 ? -1 : r.nextBoolean() ? 1 : -1) * r.nextInt(10)
+            swing.frame.setLocation(p)
+            System.err.print('!')
+        }
+    }).start()
 }
 
 def recv() {
     def robot = new Robot()
+    def prevText = ""
+    def sameTextCount = 0
     while (true) {
         def result
         def attempt = 0
@@ -93,7 +110,7 @@ def recv() {
                 break
             } catch (ReaderException e) {
                 System.err.print("z")
-                if (attempt > 30) {
+                if (attempt > 60) {
                     throw e
                 }
                 attempt++
@@ -101,21 +118,33 @@ def recv() {
             }
         }
 
+        def nextText = result.getText()
+        if (prevText == nextText) {
+            robot.delay(200)
+            sameTextCount++
+            if (sameTextCount < 10)
+                continue
+            System.err.print("!")
+        }
+        sameTextCount = 0
+
         robot.mouseMove(
             (int)result.getResultPoints().collect { point -> point.getX() }.average(),
             (int)result.getResultPoints().collect { point -> point.getY() }.average())
-        for (n in 1..2) {
-            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK)
-            robot.delay(50)
-            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK)
-            robot.delay(100)
-        }
+        robot.mousePress(InputEvent.BUTTON1_DOWN_MASK)
+        robot.delay(100)
+        robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK)
+        robot.delay(400)
 
-        def text = result.getText()
-        if (text == "\004") {
+        if (prevText == nextText)
+            continue
+        prevText = nextText
+
+        if (nextText == "\004") {
             break
         } else {
-            print(new String(result.getText().decodeBase64()))
+            def bytes = nextText.decodeBase64()
+            System.out.write(bytes, 0, bytes.length)
             System.err.print(".")
         }
     }
